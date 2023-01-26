824c90e08294f1bc5df6f969b24cb325f7d4aceb
HeeWon Lim <fake_inzelee@naver.com>
2023-01-27 00:40:31 +0900

    230127_1246

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const searchEl = document.querySelector('.search'); /* 클래스가 search인 element를 찾아서 searchEl에 할당 */
+const searchInputEl = searchEl.querySelector('input'); /* searchEl안에서 input요소를 찾는 개념 */
+
+/* searchEl에 클릭 이벤트를 추가 */
+/* 클릭을 하면 함수를 실행할 것이고 그 함수를 핸들러라고 한다 */
+searchEl.addEventListener('click', );
+
+07. 헤더와 드롭다운 메뉴 / 1218
